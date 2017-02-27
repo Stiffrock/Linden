862269a2e8751a851b12a1a5bb1a1eb61030b2ae
Pandamonium-san <henrik.p95@gmail.com>
2017-02-27 15:41:36 +0100

    Add basic methods to World.cs

--- Lindenmayer's Defense/World.cs
+++ Lindenmayer's Defense/World.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,28 @@ using System.Threading.Tasks;
 
 namespace Lindenmayers_Defense
 {
-    class World
+  class World
+  {
+    List<GameObject> gameObjects;
+    public World()
     {
+      gameObjects = new List<GameObject>();
     }
+    public void Update(GameTime gt)
+    {
+
+    }
+    public void Draw(SpriteBatch sb)
+    {
+
+    }
+    public void PostGameObject(GameObject go)
+    {
+      gameObjects.Add(go);
+    }
+    public List<GameObject> GetGameObjects()
+    {
+      return gameObjects;
+    }
+  }
 }
