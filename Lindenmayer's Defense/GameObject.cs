bd896d5332d9b74b5e431b9e9bdf959deb6c84ae
Stiffrock <christofer.malmberg@gmail.com>
2017-02-27 15:36:28 +0100

    Added some inheritance

--- Lindenmayer's Defense/GameObject.cs
+++ Lindenmayer's Defense/GameObject.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Lindenmayer_s_Defense
 {
     class GameObject
     {
+        protected Vector2 pos;
+
+        public GameObject(Vector2 pos)
+        {
+
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
     }
 }
--- Lindenmayer's Defense/Projectile.cs
+++ Lindenmayer's Defense/Projectile.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Lindenmayer_s_Defense
 {
     class Projectile : GameObject
     {
+        public Projectile(Vector2 pos) : base(pos)
+        {
+            this.pos = pos;
+        }     
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
     }
 }
