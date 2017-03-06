715c76f234cc64e345d61f49dea2882b5ce05f17
Stiffrock <christofer.malmberg@gmail.com>
2017-03-06 11:58:02 +0100

    Projectile velocity
    Added setvelocity, skewvelocity and skewangle

--- Lindenmayer's Defense/Projectile.cs
+++ Lindenmayer's Defense/Projectile.cs
@@ -19,11 +19,11 @@ namespace Lindenmayers_Defense
     protected Vector2 TurnRate { get; set; }
     protected float Speed { get; set; }
     protected float Damage { get; set; }
-    protected bool Seeking { get; set; }
+    protected bool bIsTargetSeeking { get; set; }
     protected double Lifetime { get; set; }
-    protected Vector2 targetPos;
+
     protected BoundingSphere aggroRange;
-    protected float Radius;
+    protected float aggroRadius;
     protected Random rand;
     protected World world;
     protected int MaxAccuracyOffset;
@@ -38,15 +38,18 @@ namespace Lindenmayers_Defense
       this.Damage = damage;
       MaxAccuracyOffset = 200;
       Lifetime = 3.0f;
-      Radius = tex.Width * 2;
       hitbox.Width -= tex.Width / 2;
       hitbox.Height -= tex.Height / 2;
-      aggroRange = new BoundingSphere(new Vector3(origin.X, origin.Y, 0f), Radius);
+      aggroRadius = tex.Width * 2;
+      aggroRange = new BoundingSphere(new Vector3(origin.X, origin.Y, 0f), aggroRadius);
       drawHitbox = false;
-      Seeking = false;
+      bIsTargetSeeking = false;
       Scale = 0.05f;
+      Speed = 150.0f;
       CalculateAccuracy();
+
     }
+
 
     /// <summary>
     /// Based on max accuracy being 100% meaning correct projectile target. Using projectiles accuracy as base for how close to the target it will land.
@@ -56,13 +59,14 @@ namespace Lindenmayers_Defense
     {
       rand = new Random();
       int Negative = rand.Next(0, 2);
-      float acc = rand.Next((int)75, 100) - 100;
-      
+      float acc = rand.Next((int)85, 100) - 100;
+
       if (Negative == 1)
         acc *= -1.0f;
-
-      //target = Vector2.Transform(target, Matrix.CreateRotationZ(acc * (float)(Math.PI / 180)));
       rotation = acc * (float)(Math.PI / 180);
+
+      
+
     }
 
     private void CheckForCollision()
@@ -98,18 +102,24 @@ namespace Lindenmayers_Defense
       }
     }
 
-    private void MoveTo(GameTime gt)
+    protected void SetVelocity()
     {
-      if (pos != targetPos)
-      {
-        Vector2 dir = targetPos - this.pos;
-        dir.Normalize();
-        this.pos += dir * Speed * (float)gt.ElapsedGameTime.TotalSeconds;
-      }
-
-      if (GetDistanceToTarget(targetPos) <= 20)
-        Die();
+      Velocity = Forward() * Speed;
     }
+
+    protected void SkewVelocity(Vector2 vel, GameTime gt)
+    {
+      Velocity += vel * (float)gt.ElapsedGameTime.TotalSeconds;
+    }
+
+    protected void SkewDirection(float angle, GameTime gt)
+    {
+      rotation +=  angle * (float)(Math.PI / 180) * (float)gt.ElapsedGameTime.TotalSeconds;
+    }
+
+
+
+
     protected double GetDistanceToTarget(Vector2 target)
     {
       float x = pos.X - target.X;
@@ -120,12 +130,15 @@ namespace Lindenmayers_Defense
     public override void Update(GameTime gt)
     {
       base.Update(gt);
+      SetVelocity();
+      SkewVelocity(new Vector2(0.0f, 0.0f), gt);
+      SkewDirection(-45.0f, gt);
+
       Lifetime -= gt.ElapsedGameTime.Seconds;
       if (Lifetime <= 0)
       {
         Die();
       }
-      MoveTo(gt);
       CheckForCollision();
     }
     public override void Draw(SpriteBatch sb)
