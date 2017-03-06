05db5d254354065378869210b9b19ff7371e3f40
Pandamonium-san <henrik.p95@gmail.com>
2017-03-06 14:29:32 +0100

    Add particle manager

---
+++ Lindenmayer's Defense/Particle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Lindenmayers_Defense
+{
+  class Particle : GameObject
+  {
+    float initialAlpha;
+    double lifeTime;
+    double elapsedTime;
+    bool fades;
+    public Particle(Texture2D tex, Vector2 pos, float lifeTime, Color color, float alpha, bool fades) : base(tex, pos)
+    {
+      this.color = color;
+      this.alpha = alpha;
+      initialAlpha = alpha;
+      this.lifeTime = lifeTime;
+      this.fades = fades;
+      elapsedTime = 0;
+    }
+    public override void Update(GameTime gt)
+    {
+      elapsedTime += gt.ElapsedGameTime.Milliseconds;
+      if (fades)
+        alpha = (float)((1 - elapsedTime / lifeTime) * initialAlpha);
+      if (elapsedTime >= lifeTime)
+        Die();
+    }
+    public override void Draw(SpriteBatch sb)
+    {
+      base.Draw(sb);
+    }
+  }
+}
---
+++ Lindenmayer's Defense/ParticleManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Lindenmayers_Defense
+{
+  class ParticleManager
+  {
+    List<Particle> particles;
+
+    public ParticleManager()
+    {
+      particles = new List<Particle>();
+    }
+
+    public void CreateExplosion(Vector2 pos, float radius, Color color, float duration)
+    {
+      Particle exp = new Particle(AssetManager.GetTexture("dot"), pos, duration, color, 0.8f, true);
+      exp.Scale = radius / 512.0f;
+      particles.Add(exp);
+    }
+
+    public void Update(GameTime gt)
+    {
+      for (int i = 0; i < particles.Count; i++)
+      {
+        particles[i].Update(gt);
+      }
+      particles.RemoveAll(p => p.Disposed);
+    }
+
+    public void Draw(SpriteBatch sb)
+    {
+      for (int i = 0; i < particles.Count; i++)
+      {
+        particles[i].Draw(sb);
+      }
+    }
+  }
+}
